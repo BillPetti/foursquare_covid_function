029ce4f787e89a055080acaa84304f35e694ae2b
BillPetti <billpetti@gmail.com>
2020-05-11 09:17:21 -0400

    updated to now pull county data and index results

--- acquire_foursquare_covid_data.R
+++ acquire_foursquare_covid_data.R
@@ -3,13 +3,13 @@ acquire_foursquare_covid_data <- function(aggregate = 'state',
                                           state_name = NA,
                                           index = FALSE) {
 
-  if (aggregate != 'state')
+  if (aggregate == 'state' && !is.na(state_name))
 
-    stop(message('Due to the change in the data structure, County level data cannot be pulled with this function at this time'))
+    stop(message('Do not supply a state name if aggregate is state level'))
 
   if (index == TRUE)
 
-    message('Data will be indexed based on the average traffic for the given day of the week across February. Only datat for all demographics will be returned.')
+    message('Data will be indexed based on the average traffic for the given day of the week across February. \nOnly data for all demographics will be returned.')
 
   fsqr_state_names <- c("Alabama", "Alaska", "Arizona",
                         "Arkansas", "California", "Colorado", "Connecticut", "Delaware",
@@ -25,22 +25,45 @@ acquire_foursquare_covid_data <- function(aggregate = 'state',
 
   scale_fs_data <- function(payload) {
 
-    anchor <- payload %>%
-      filter(demo == "All") %>%
-      mutate(day_number = lubridate::wday(date)) %>%
-      filter(date <= '2020-02-29') %>%
-      group_by(state, categoryname, day_number) %>%
-      summarise(avg_traffic = mean(visits)) %>%
-      ungroup()
+    if(aggregate != 'state') {
 
-    payload_indexed <- payload %>%
-      filter(demo == "All") %>%
-      mutate(day_number = lubridate::wday(date)) %>%
-      left_join(anchor, by = c('state', 'day_number', 'categoryname')) %>%
-      mutate(index_visits = round(100 * (visits/avg_traffic))) %>%
-      group_by(state, categoryname, day_number) %>%
-      mutate(visits_7_day_roll_ave = zoo::rollapply(index_visits, 7, mean, align = 'right', fill = NA)) %>%
-      ungroup()
+      anchor <- payload %>%
+        filter(demo == "All") %>%
+        mutate(day_number = lubridate::wday(date)) %>%
+        filter(date <= '2020-02-29') %>%
+        group_by(state, county, categoryid, categoryname, day_number) %>%
+        summarise(avg_traffic = mean(visits)) %>%
+        ungroup()
+
+      payload_indexed <- payload %>%
+        filter(demo == "All") %>%
+        mutate(day_number = lubridate::wday(date)) %>%
+        left_join(anchor, by = c('state', 'day_number', 'county', 'categoryid', 'categoryname')) %>%
+        mutate(index_visits = round(100 * (visits/avg_traffic))) %>%
+        group_by(state, county, categoryid, categoryname) %>%
+        mutate(visits_7_day_roll_ave = zoo::rollapply(index_visits, 7, mean, align = 'right', fill = NA)) %>%
+        ungroup()
+
+    } else {
+
+      anchor <- payload %>%
+        filter(demo == "All") %>%
+        mutate(day_number = lubridate::wday(date)) %>%
+        filter(date <= '2020-02-29') %>%
+        group_by(state, categoryname, day_number) %>%
+        summarise(avg_traffic = mean(visits)) %>%
+        ungroup()
+
+      payload_indexed <- payload %>%
+        filter(demo == "All") %>%
+        mutate(day_number = lubridate::wday(date)) %>%
+        left_join(anchor, by = c('state', 'day_number', 'categoryname')) %>%
+        mutate(index_visits = round(100 * (visits/avg_traffic))) %>%
+        group_by(state, categoryname) %>%
+        mutate(visits_7_day_roll_ave = zoo::rollapply(index_visits, 7, mean, align = 'right', fill = NA)) %>%
+        ungroup()
+
+    }
 
     return(payload_indexed)
   }
@@ -113,30 +136,90 @@ acquire_foursquare_covid_data <- function(aggregate = 'state',
 
     }
 
-  } else if (aggregate != 'state' & type == 'grouped') {
-
-    state_name <- stringr::str_to_title(state_name)
-    state_name <- gsub("[[:space:]]", "", state_name)
-
-    build_url <- paste0('https://visitdata.org/data/grouped', state_name, '.csv')
-
-    payload <- vroom::vroom(build_url)
-
-    names(payload) <- c('date', 'state', 'county', 'category', 'all_ages',
-                        'under_65', 'over_65', 'cat_num')
   } else {
 
-    state_name <- stringr::str_to_title(state_name)
-    state_name <- gsub("[[:space:]]", "", state_name)
+    generate_county_urls <- function(state_name = NA) {
 
-    #build_url <- paste0('https://data.visitdata.org/processed/vendor/foursquare/asof/', date, '-v0/raw', state_name, '.csv')
+      url <- 'https://visitdata.org/data'
 
-    build_url <- paste0('https://visitdata.org/data/raw', state_name, '.csv')
+      payload <- read_html(url)
 
-    payload <- vroom::vroom(build_url)
+      urls <- payload %>%
+        html_nodes('a') %>%
+        html_attr('href') %>%
+        as.data.frame() %>%
+        rename(url_slug = '.') %>%
+        mutate(full_url = paste0('https://visitdata.org', url_slug)) %>%
+        .[-c(1:2),]
 
-    names(payload) <- c('date', 'state', 'county', 'cat_code', 'category',
-                        'all_ages', 'under_65', 'over_65', 'cat_num')
+      text <- payload %>%
+        html_nodes('a') %>%
+        html_attr('href') %>%
+        as.data.frame() %>%
+        rename(code_text = '.') %>%
+        mutate(code_text = as.character(code_text)) %>%
+        .[-c(1,2), 1]
+
+      state <- payload %>%
+        html_nodes('a') %>%
+        html_attr('href') %>%
+        as.data.frame() %>%
+        rename(state_text = '.') %>%
+        mutate(state_text = as.character(state_text)) %>%
+        mutate(state_text = gsub("/data/|.csv", '', state_text)) %>%
+        mutate(state_text = gsub('_(.*)', '', state_text)) %>%
+        .[-c(1,2),]
+
+      if(!is.na(state_name)) {
+
+        if(grepl(" ", state_name)) {
+
+          state_name <- stringr::str_to_title(state_name)
+          state_name <- gsub("[[:space:]]", "", state_name)
+
+        }
+
+        url_payload <- tibble(url = urls$full_url,
+                              text = text,
+                              state = state) %>%
+          filter(grepl('County', text)) %>%
+          filter(state_name == state)
+
+      } else {
+
+        url_payload <- tibble(url = urls$full_url,
+                              text = text,
+                              state = state) %>%
+          filter(grepl('County', text))
+      }
+
+      return(url_payload)
+
+    }
+
+    if(!is.na(state_name)) {
+
+      urls <- generate_county_urls(state_name)
+
+    } else {
+
+      urls <- generate_county_urls()
+    }
+
+    map_county_files <- map2_df(.x = urls$url,
+                                .y = urls$text,
+                                ~{message('Acquiring data for ', gsub("/data/|.csv", '', .y))
+                                  vroom::vroom(.x, delim = ',')})
+
+
+    names(map_county_files) <- c('date', 'state', 'county', 'categoryid',
+                                 'categoryname', 'demo', 'visits', 'avgDuration',
+                                 'p50Duration')
+
+    payload <- map_county_files
+
+    payload <- scale_fs_data(payload)
+
   }
 
   if (aggregate == 'state' & !is.na(state_name)) {
